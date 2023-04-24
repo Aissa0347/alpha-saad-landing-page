0a39d64e67f0106132df1fb888922eb272625007
Aissa Semaoui <SEMAOUI0347@GMAIL.COM>
2023-04-24 14:35:45 +0100

    projects and clients sliders

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
 import "./style.css";
 import { tns } from "tiny-slider";
 
-const slider = tns({
+const clientsSlider = tns({
   container: ".mySlider",
   items: 3,
   slideBy: "page",
   autoplay: true,
+  loop: false,
+  rewind: true,
+  controlsContainer: ".controlsContainer",
+  prevButton: ".prevBtn",
+  nextButton: ".nextBtn",
+  responsive: {
+    300: {
+      items: 2,
+      controls: true,
+      edgePadding: 30,
+    },
+    568: {
+      items: 3,
+    },
+    768: {
+      items: 4,
+    },
+    1024: {
+      items: 5,
+    },
+  },
+});
+
+const projectsSlider = tns({
+  container: ".projectsSlider",
+  items: 6,
+  // gutter: 10,
+  mouseDrag: true,
+  swipeAngle: false,
+  slideBy: "page",
+  autoplay: true,
+  loop: false,
+  rewind: true,
+  // controlsContainer: ".controlsContainer",
+  // prevButton: ".prevBtn",
+  // nextButton: ".nextBtn",
+  responsive: {
+    300: {
+      items: 1,
+    },
+    468: {
+      items: 2,
+    },
+    640: {
+      items: 3,
+    },
+    968: {
+      items: 4,
+    },
+    1280: {
+      items: 5,
+    },
+  },
 });
