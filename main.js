f1fdc191525c50e4a3025ae33b7e6ced302044cd
Abdohm-coder <hadjmedabdo@gmail.com>
2023-04-25 11:35:55 +0100

    style navbar with icons and pathname

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 import "./style.css";
 import { tns } from "tiny-slider";
 
-
 // Navbar
-const items = document.querySelector(".nav-items");
+const navItems = document.querySelector(".nav-items");
 const btn = document.querySelector(".burger");
 
 btn.addEventListener("click", () => {
-  if(items.classList.contains("hide")) items.classList.remove("hide");
-  else items.classList.add("hide");
-})
+  if (navItems.classList.contains("hide")) navItems.classList.remove("hide");
+  else navItems.classList.add("hide");
+});
+
+const items = navItems.children;
+const pathname = window.location.pathname.split("/")[1];
+
+for (let i = 0; i < items.length; i++) {
+  const a = i === 0 ? items[0].children[1] : items[i].children[0];
+  console.log(a);
+  const isEqual = a.getAttribute("href").includes(pathname);
+  if (isEqual) {
+    if (!a.classList.contains("active")) a.classList.add("active");
+  } else a.classList.remove("active");
+}
 
 // Sliders
 const clientsSlider = tns({
